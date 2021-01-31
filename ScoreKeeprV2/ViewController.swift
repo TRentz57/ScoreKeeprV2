830b7619212f2845aeab0b481857224c85d56532
Tyler Rentz <popmythumb@yahoo.com>
2021-01-31 11:47:39 -0500

    Initial Commit

---
+++ ScoreKeeprV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoreKeeprV2
+//
+//  Created by Tyler Rentz on 1/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ScoreKeeprV2Tests/ScoreKeeprV2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  ScoreKeeprV2Tests.swift
+//  ScoreKeeprV2Tests
+//
+//  Created by Tyler Rentz on 1/31/21.
+//
+
+import XCTest
+@testable import ScoreKeeprV2
+
+class ScoreKeeprV2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
